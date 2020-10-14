5d0b5c63b9ed269127a0fe0fe85cd6d859bb28c8
Albert Li <a.li@levi9.com>
2020-10-14 13:04:58 +0300

    Add parse.py

---
+++ parse.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+response = requests.get('https://ultra-shop.com/men/odezhda/p/56578;0005_4ec180?product')
+soup = BeautifulSoup(response.text, 'html.parser')
+
+
+def main():
+    ul_list = soup.find_all('ul', {'class': 'text'})
+    needed_ul = ul_list[0]
+    data = {}
+    for li in needed_ul.find_all('li'):
+        li = li.text.strip().split(':')
+        data[li[0]] = li[1]
+        print(data)
+        break
+
+
+if __name__ == '__main__':
+    main()
