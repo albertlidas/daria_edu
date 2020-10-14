14ccbed5c55f1d923903473721278b24ee05869f
Albert Li <a.li@levi9.com>
2020-10-14 14:02:32 +0300

    Split the main func

--- parse.py
+++ parse.py
@@ -5,9 +5,14 @@ response = requests.get('https://ultra-shop.com/men/odezhda/p/56578;0005_4ec180?
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
-def main():
-    ul_list = soup.find_all('ul', {'class': 'text'})
+def get_ul_tag(html_soup):
+    ul_list = html_soup.find_all('ul', {'class': 'text'})
     needed_ul = ul_list[0]
+    return needed_ul
+
+
+def main():
+    needed_ul = get_ul_tag(soup)
     data = {}
     for li in needed_ul.find_all('li'):
         li = li.text.strip().split(':')
