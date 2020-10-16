20d183b328edd5305b8cd7069dbe2a6c1059fbf0
Daria_Zaika <dashazaika@gmail.com>
2020-10-16 12:04:24 +0300

    Code has been updated to return all items from li tag

--- parse.py
+++ parse.py
@@ -5,16 +5,20 @@ response = requests.get('https://ultra-shop.com/men/odezhda/p/56578;0005_4ec180?
 soup = BeautifulSoup(response.text, 'html.parser')
 
 
+def get_ul_tag(html_soup):
+    ul_list = html_soup.find_all('ul', {'class': 'text'})
+    needed_ul = ul_list[0]
+    return needed_ul
+
 def main():
-    """main function"""
-    ul_list = soup.find_all('ul', {'class': 'text'})
-    needed_ul = ul_list[0]#new item
+    needed_ul = get_ul_tag(soup)
     data = {}
     for li in needed_ul.find_all('li'):
         li = li.text.strip().split(':')
-        data[li[0]] = li[1]
+        key = li[0]
+        value = li[1].strip()
+        data = {key:value}
         print(data)
-        break
 
 
 if __name__ == '__main__':
