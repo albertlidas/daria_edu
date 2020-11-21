9fc6421daa2b6d7ef8629953d4ce5de368c6cac5
Daria_Zaika <dashazaika@gmail.com>
2020-11-21 19:09:05 +0200

    html templates adding

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,13 +1,29 @@
 from django.db import models
+#from django.contrib.auth.models import User
 
-class User(models.Model):
+
+class Author(models.Model):
     name = models.CharField
-    age = models.IntegerField
+    birthday = models.DateField
+
+    def __str__(self):
+        return self.name
+
 
 class Post (models.Model):
-    author = models.ForeignKey(User, on_delete=models.PROTECT)
+    author = models.ForeignKey(Author, on_delete=models.PROTECT)
     pub_date = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=35)
+    post_content = models.CharField
+
+    def __str__(self):
+        return self.title
+
 
 class Comment(models.Model):
-    username = models.ForeignKey(User, on_delete=models.PROTECT)
+    username = models.ForeignKey(Author, on_delete=models.PROTECT)
     pub_date = models.DateTimeField(auto_now_add=True)
+    comment_text = models.CharField(max_length=200)
+
+    def __str__(self):
+        return '{} + ' ' + {}'.format(self.username, self.comment_text)
