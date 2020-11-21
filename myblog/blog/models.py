f26cb7cf34a68479518c1f860366c3385a9efb71
Daria_Zaika <dashazaika@gmail.com>
2020-11-21 23:53:41 +0200

    updating models and adding static files

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,29 +1,30 @@
 from django.db import models
-#from django.contrib.auth.models import User
+#from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 class Author(models.Model):
-    name = models.CharField
-    birthday = models.DateField
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        return str(self.user)
 
 
 class Post (models.Model):
-    author = models.ForeignKey(Author, on_delete=models.PROTECT)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
     title = models.CharField(max_length=35)
-    post_content = models.CharField
+    article = models.TextField(max_length=1500, blank=True)
 
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
-    username = models.ForeignKey(Author, on_delete=models.PROTECT)
+    username = models.ForeignKey(Author, on_delete=models.CASCADE)
     pub_date = models.DateTimeField(auto_now_add=True)
-    comment_text = models.CharField(max_length=200)
+    comment_text = models.TextField(max_length=35, null=True)
+    post_for_comment = models.ForeignKey(Post, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
-        return '{} + ' ' + {}'.format(self.username, self.comment_text)
+        return ('{}' + ' : ' + '{}').format(self.username, self.comment_text[:20])
