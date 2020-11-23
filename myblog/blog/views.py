c76d4e9afd481e15aede925b6cd1a15bfb4ec71a
Daria_Zaika <dashazaika@gmail.com>
2020-11-23 14:29:09 +0200

    cannot pass variable to be included template index.html

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.template.response import TemplateResponse
+
+
+from .models import Post
 
 
 def index(request):
-    return render(request, 'blog/index.html')
+    context = {'title': Post.title, 'article': Post.article}
+    return render(request, 'blog/index.html', context)
