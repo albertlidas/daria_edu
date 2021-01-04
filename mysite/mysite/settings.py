ccade530e4b43572d007a800ac571c60fa50e560
Daria_Zaika <dashazaika@gmail.com>
2021-01-04 15:09:53 +0200

    password reset confirm adding

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -135,3 +135,10 @@ STATICFILES_DIRS = [
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'dashazaika@gmail.com'
+EMAIL_HOST_PASSWORD = ''
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -26,7 +26,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('signup/', signup, name='signup'),
     path('login/', logIn, name='login'),
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='passward_reset'),
-    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='passward_reset_confirm'),
-    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='passward_reset_done'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name='password_reset'),
+    path('password_reset_confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html', ), name='password_reset_confirm'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
 ]
