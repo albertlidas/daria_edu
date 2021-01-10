146ffbf4bc4370204d27d5294138270896bc678e
Daria_Zaika <dashazaika@gmail.com>
2021-01-10 22:28:42 +0200

    update comment func

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path, re_path
-from .views import HomeView, PostDetailView, PostCreateView
-from . import views
+from .views import HomeView, PostDetailView, PostCreateView, UpdatePostView, DeletePostView, add_comment
+
 
 urlpatterns = [
     path(r'', HomeView.as_view(), name='post_list'),
     re_path(r'^(?P<pk>\d+)/$', PostDetailView.as_view(), name='post_detail'),
     re_path(r'^add/$', PostCreateView.as_view(), name='article_add'),
+    re_path(r'^(?P<pk>\d+)/update/$', UpdatePostView.as_view(), name='post_update'),
+    re_path(r'^(?P<pk>\d+)/delete/$', DeletePostView.as_view(), name='post_delete'),
+    re_path(r'^(?P<pk>\d+)/comment/$', add_comment, name='new_comment'),
 ]
--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,8 @@
 from .models import Post, Category, Comment
 from .forms import CommentForm
-from django.views.generic import DetailView, ListView, CreateView
-from django.shortcuts import render, get_object_or_404
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 class HomeView(ListView):
     model = Post
@@ -20,28 +21,58 @@ class PostDetailView(DetailView):
     template_name = 'post_detail.html'
     context_object_name = 'post_by_category'
 
-    def comment_form(self, request):
-        if request.method == 'POST':
-            comment_form = CommentForm(request.POST)
-            if comment_form.is_valid():
-                new_comment = comment_form.save()
-                new_comment.save()
-            else:
-                comment_form = CommentForm()
-        return comment_form
-
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         post = get_object_or_404(Post, id=self.kwargs['pk'])
         context['comments'] = Comment.objects.filter(post=post).order_by('-id')
         context['art_category'] = Post.objects.filter(category=self.kwargs['pk'])
-        context['comment_form'] = self.comment_form
         return context
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['category', 'author', 'title', 'article']
 
     def form_valid(self, form):
         form.instance.Author = self.request.user
         return super().form_valid(form)
+
+class UpdatePostView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['category', 'author', 'title', 'article']
+
+    def form_valid(self, form):
+        form.instance.Author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+class DeletePostView (LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/'
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
+
+def add_comment (request, pk):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, pk=post_id)
+    comments = Comment.objects.filter(is_active=True)
+    new_comment = None
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+    return render(request, 'add_comment.html', {'post': post, 'comments': comments, 'new_comment': new_comment, 'comment_form': comment_form})
+
+
